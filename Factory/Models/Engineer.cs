79b6856fc94265aa804e956b45de5e02b3075ad3
Rafael Petrachini <raffp@me.com>
2023-01-12 13:17:50 -0800

    add initial file setup and controllers for Engineers and Machines

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
